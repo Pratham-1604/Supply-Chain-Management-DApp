2564fd332c0f5cd8633b5ec5dade3f10ccc4816c
Prathamesh Mundada <mundadaprathamesh@gmail.com>
2024-05-08 02:38:57 +0530

    navbar done

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { TrackingProvider } from "../Context/TrackingContext";
 import NavBar from "../Components/NavBar";
 import Footer from "../Components/Footer";
-
+import "../styles/globals.css";
 export default function App({ Component, pageProps }) {
   return (
     <>
