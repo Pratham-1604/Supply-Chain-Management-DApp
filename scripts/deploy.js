f849f30948d54e5374e7dd5e7e85a6d9b8524afd
Prathamesh Mundada <mundadaprathamesh@gmail.com>
2024-05-07 20:45:21 +0530

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,20 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through node <script>.
+//
+// You can also run a script with npx hardhat run <script>. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+// deploy.js
+const { ethers } = require("hardhat");
+
+async function deployContracts() {
+  const Tracking = await ethers.getContractFactory("Tracking");
+  const tracking = await Tracking.deploy();
+  await tracking.deployed();
+  console.log("Tracking Contract Address:", tracking.address);
+  
+}
+deployContracts().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
